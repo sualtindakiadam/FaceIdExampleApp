5577e40d38dfb115d87c302540561cb54944c0fd
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-19 14:32:10 +0300

    faceId example V1 fnished

--- FaceIdExampleSwiftApp/ViewController.swift
+++ FaceIdExampleSwiftApp/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func singInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){ // deviceOwnerAuthenticationWithBiometrics kontrolü biometrik yani parmak izi veya yüz tanıma için seçilir
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in// info pliste face id ekle
+                
+                //face id test etmek için simulatörde üst menüde Features - Face ID - Enrolled seç ve yine aynı menüde test etmek için face id eşleşti eşleşmedi seçenekleriyle de kontrol et
+                if success == true {
+                    
+                    //successfull
+                    DispatchQueue.main.async { // arka planda kullanıcı arayüzünü etkileyecek bir iş yapıyorsak işlemi farklı bir thread da yapmak gerekir.
+                              self.performSegue(withIdentifier: "signedVC", sender: nil)
+                    }
+              
+                }else{
+                    DispatchQueue.main.async {
+                        self.signInLabel.text = "Error"
+                    }
+                    
+                    
+                }
+            
+            }
+            
+        }
+        
+        
+    }
+    
 }
 
